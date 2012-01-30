15b4f82172ebe448b5c78036e80abe7c9c7e65df
Taavi <Taavi@taavi.icosagen.local>
2012-01-30 19:53:42 +0200

    Parameters added to program page
    Checkboxes and fields if necessary. Need to be connected with post.

--- bioCloud/biocloud/static/bioCloud.js
+++ bioCloud/biocloud/static/bioCloud.js
@@ -58,6 +58,13 @@ biocloud = {
         });
         
     },
+    
+    setParameters: function(anArrayOfParameterSpecs) {
+        this.parameterSpecs = anArrayOfParameterSpecs;
+        this.parameters = jQuery.map(anArrayOfParameterSpecs, function(each){
+            return new biocloud.Parameter(each);
+        })
+    },
     updateProgramBoxFor: function(aProgramBox, aProgramName, sequenceNumber){
         var program = undefined;
         jQuery.each(biocloud.programs, function(i, each){
@@ -77,6 +84,7 @@ biocloud = {
         this.name = aProgramSpec.name;
         this.homepage = aProgramSpec.homepage;
         this.files = [];
+        this.parameters = [];
         for(i=0; i < aProgramSpec.inputs; i++){
             this.files
                 .push(new biocloud.InputFile(i,
@@ -86,8 +94,14 @@ biocloud = {
             this.files
                 .push(new biocloud.OutputFile(i+1, aProgramSpec.inputs + i,
                         aProgramSpec.fileNames[aProgramSpec.inputs + i]));
+                        
         }
         /* What to do about parameters? */
+        for(i=0;i < aProgramSpec.parameters.length; i++){
+            this.parameters
+                .push(new biocloud.Parameter(i,aProgramSpec.parameters[i][0],
+                    aProgramSpec.parameters[i][1],aProgramSpec.parameters[i][2]));
+        }
     },
     InputFile: function(index, name){
         this.index = index+1;
@@ -98,6 +112,12 @@ biocloud = {
         this.index = index;
         this.name = name;
         this.fieldIndex = fieldIndex;
+    },
+    Parameter: function(index,name,flag,flagtype){
+        this.index = index;
+        this.name = name;
+        this.flag = flag;
+        this.flagtype = flagtype;
     }
 };
 
@@ -108,9 +128,19 @@ biocloud.Program.prototype = {
         jQuery.each(this.files, function(i, each){
             each.renderTo($("<li></li>").appendTo(aBox), id)
         })
+
         this.command = $('<input type="text" class="commandline" readonly="readonly" name="program.'+id+'.commandPreview" />')
             .appendTo($("<li></li>")
                 .appendTo(aBox));
+        /* Taavi testing parameters */
+        aBox.append("Choose parameters<br />")
+         jQuery.each(this.parameters, function(i, each){
+             aBox.append(each.name);
+             $('<input type="checkbox" name="program.'+id+'.parameter.'+each.index+'.flag" value="'+each.flag+'" checked>').appendTo($("<li></li>").appendTo(aBox));
+             if (each.flagtype=="var") {
+                 $('<input type="text" class="parameter" name="program.'+id+'.parameter.'+each.index+'.value" />').appendTo($("<li></li>").appendTo(aBox));
+             }
+        })
     }
 };
 // create a block context, to avoid cluttering the global namespace with the fileprototype
@@ -169,6 +199,7 @@ $(document).ready(function(event){
         event.preventDefault();
         $.getJSON('/xhr/'+this.value+'/content', function(data){
             biocloud.setFiles(data)
+            biocloud.setParameters(data)
         });
     });
 });
